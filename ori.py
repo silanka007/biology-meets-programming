44e5428c8ea63029c6adc5dc80c137b2a9986900
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2021-03-29 11:31:56 +0100

    reading txt files in python

---
+++ ori.py
@@ -0,0 +1,25 @@
+# In the rest of this chapter, we will focus on the relatively easy case of
+#  finding ori in bacterial genomes, most of which consist of a single 
+# circular chromosome. Research has shown that the region of the bacterial 
+# genome encoding ori is typically a few hundred nucleotides long. Our plan
+#  is to begin with a bacterium in which ori is known, and then determine
+#  what makes this genomic region special in order to design a computational
+#  approach for finding ori in other bacteria. Our example is Vibrio cholerae,
+#  the pathogenic bacterium that causes cholera; here is the nucleotide
+#  sequence appearing in the ori of Vibrio cholerae:
+
+# atcaatgatcaacgtaagcttctaagcatgatcaaggtgctcacacagtttatccacaac
+#  ctgagtggatgacatcaagataggtcgttgtatctccttcctctcgtactctcatgacca
+#  cggaaagatgatcaagagaggatgatttcttggccatatcgcaatgaatacttgtgactt
+#  gtgcttccaattgacatcttcagcgccatattgcgctggccaaggtgacggagcgggatt
+#  acgaaagcatgatcatggctgttgttctgtttatcttgttttgactgagacttgttagga
+#  tagacggtttttcatcactgactagccaaagccttactctgcctgacatcgaccgtaaat
+#  tgataatgaatttacatgcttccgcgacgatttacctcttgatcatcgatccgattgaag
+#  atcttcaattgttaattctcttgcctcgactcatagccatgatgagctcttgatcatgtt
+#  tccttaaccctctattttttacggaagaatgatcaagctgctgctcttgatcatcgtttc
+
+dna = ""
+with open("./cholera.txt", "r") as sequence:
+  dna = sequence.read()
+
+print(dna)
