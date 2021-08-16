96a5cc76e867a49b98791a44aee740f8b462bffd
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2021-08-16 16:39:55 +0100

    probability functionimplemented

---
+++ week-3/greedy_motifs_search/probability.py
@@ -0,0 +1,18 @@
+def Pr(text, profile):
+    probability = 1
+
+    for pos in range(len(text)):
+        nucleotide = text[pos]
+        probability *= profile[nucleotide][pos]
+
+    return probability
+
+
+if __name__ == "__main__":
+    profile = {
+        "A": [0.2, 0.2, 0.0, 0.0, 0.0, 0.0, 0.9, 0.1, 0.1, 0.1, 0.3, 0.0],
+        "T": [0.1, 0.6, 0.0, 0.0, 0.0, 0.0, 0.0, 0.4, 0.1, 0.2, 0.4, 0.6],
+        "G": [0.0, 0.0, 1.0, 1.0, 0.9, 0.9, 0.1, 0.0, 0.0, 0.0, 0.0, 0.0],
+        "C": [0.7, 0.2, 0.0, 0.0, 0.1, 0.1, 0.0, 0.5, 0.8, 0.7, 0.3, 0.4],
+    }
+    print(Pr("ACGGGGATTACC", profile))
